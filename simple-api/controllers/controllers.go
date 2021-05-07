cebb49b9c17abf2e7cd408b02d7b7f743ad07ee6
Jamie <james_wooltorton@hotmail.com>
2021-05-07 11:42:52 +0100

    Cleaned up Source api

--- simple-api/controllers/controllers.go
+++ simple-api/controllers/controllers.go
@@ -2,9 +2,8 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
+	"strconv"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -17,8 +16,20 @@ const (
     dsn = "host=localhost user=postgres password=changeme dbname=postgres port=5432 sslmode=disable"
 )
 
+func respondWithError(w http.ResponseWriter, code int, message string) {
+    respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+    response, _ := json.Marshal(payload)
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(code)
+    w.Write(response)
+}
+
 func GetSources(w http.ResponseWriter, r *http.Request){
-	fmt.Println("Endpoint Hit: GetSources")
+	// fmt.Println("Endpoint Hit: GetSources")
 
     db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
     if err != nil {
@@ -27,13 +38,16 @@ func GetSources(w http.ResponseWriter, r *http.Request){
     // defer db.Close()
 
     var sources []dataTypes.Source
-    db.Find(&sources)
+    if result := db.Find(&sources); result.Error != nil {
+        respondWithError(w, http.StatusInternalServerError, result.Error.Error())
+        return
+    }
 
-    json.NewEncoder(w).Encode(sources)
+    respondWithJSON(w, http.StatusOK, sources)
 }
 
 func GetSource(w http.ResponseWriter, r *http.Request){
-	fmt.Println("Endpoint Hit: GetSource")
+	// fmt.Println("Endpoint Hit: GetSource")
 
     vars := mux.Vars(r)
     id := vars["id"]
@@ -45,35 +59,70 @@ func GetSource(w http.ResponseWriter, r *http.Request){
     // defer db.Close()
 
     var source dataTypes.Source
-    if err := db.First(&source, id).Error; err != nil {
-        w.WriteHeader(http.StatusNoContent)
-        json.NewEncoder(w).Encode(err)
+    if result := db.First(&source, id); result.Error != nil {
+        respondWithError(w, http.StatusNotFound, result.Error.Error())
         return
-      }
-
-    json.NewEncoder(w).Encode(source)
-}
+    }
+    respondWithJSON(w, http.StatusOK, source)
+} 
 
 func CreateSource(w http.ResponseWriter, r *http.Request) { 
-	fmt.Println("Endpoint Hit: CreateSource")  
-
+	// fmt.Println("Endpoint Hit: CreateSource")  
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
     if err != nil {
         panic("failed to connect database")
     }
     // defer db.Close()
 
-    reqBody, _ := ioutil.ReadAll(r.Body)
-    var source dataTypes.Source 
-    json.Unmarshal(reqBody, &source)
+    var source  dataTypes.Source
+    decoder := json.NewDecoder(r.Body)
 
-    db.Create(&dataTypes.Source{Name: source.Name, Desc: source.Desc, Endpoint: source.Endpoint })
+    if err := decoder.Decode(&source); err != nil {
+        respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+        return
+    }
+    defer r.Body.Close()
 
-    fmt.Fprintf(w, "New Source Successfully Created")
+    if result := db.Create(&dataTypes.Source{Name: source.Name, Desc: source.Desc, Endpoint: source.Endpoint, SourceTypeID: source.SourceTypeID }); result.Error != nil {
+        respondWithError(w, http.StatusInternalServerError, result.Error.Error())
+        return
+    }
+    respondWithJSON(w, http.StatusCreated, source)
 }
 
 func UpdateSource(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: UpdateSource")
+	// fmt.Println("Endpoint Hit: UpdateSource")
+
+    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        panic("failed to connect database")
+    }
+    
+    vars := mux.Vars(r)
+    id, err := strconv.Atoi(vars["id"])
+    if err != nil {
+        respondWithError(w, http.StatusBadRequest, "Invalid source ID")
+        return
+    }
+
+    var s dataTypes.Source
+    decoder := json.NewDecoder(r.Body)
+    if err := decoder.Decode(&s); err != nil {
+        respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+        return
+    }
+    defer r.Body.Close()
+    s.ID = uint(id)
+
+    if err := db.Save(&s).Error; err != nil {
+        respondWithError(w, http.StatusInternalServerError, err.Error())
+        return
+    }
+    respondWithJSON(w, http.StatusOK, s)
+}
+
+func DeleteSource(w http.ResponseWriter, r *http.Request) {
+	// fmt.Println("Endpoint Hit: DeleteSource")
 
     db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
     if err != nil {
@@ -81,78 +130,55 @@ func UpdateSource(w http.ResponseWriter, r *http.Request) {
     }
 
     vars := mux.Vars(r)
-    id := vars["id"]
-
-	var updatedEvent dataTypes.Source
-	reqBody, _ := ioutil.ReadAll(r.Body)
-	json.Unmarshal(reqBody, &updatedEvent)
-
-    var source dataTypes.Source
-    db.First(&source, id)
-
-    source.Name = updatedEvent.Name 
-    source.Desc = updatedEvent.Desc
-    source.Endpoint = updatedEvent.Endpoint
-
-    db.Save(&source)
-
-	json.NewEncoder(w).Encode(source)
-}
-
-func DeleteSource(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: DeleteSource")
-
-    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    id, err := strconv.Atoi(vars["id"])
     if err != nil {
-        panic("failed to connect database")
+        respondWithError(w, http.StatusBadRequest, "Invalid Product ID")
+        return
     }
-
-	vars := mux.Vars(r)
-	id := vars["id"]
-
-    var source dataTypes.Source
-    db.First(&source, id)
-	db.Delete(&source)
-
-	fmt.Fprintf(w, "Successfully Deleted Source")
+    
+    if err := db.Delete(&dataTypes.Source{}, id).Error; err != nil {
+        respondWithError(w, http.StatusInternalServerError, err.Error())
+        return
+    }
+    respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
 }
 
 
 // SouceTypes
-func GetProducts(w http.ResponseWriter, r *http.Request){
-	fmt.Println("Endpoint Hit: GetProducts")
+// func GetProducts(w http.ResponseWriter, r *http.Request){
+// 	fmt.Println("Endpoint Hit: GetProducts")
 
-	// db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
-    // dsn := "host=localhost user=postgres password=changeme dbname=postgres port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if err != nil {
-        panic("failed to connect database")
-    }
-    // defer db.Close()
-    // db.Close()
+// 	// db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+//     // dsn := "host=localhost user=postgres password=changeme dbname=postgres port=5432 sslmode=disable"
+// 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+//     if err != nil {
+//         panic("failed to connect database")
+//     }
+//     // defer db.Close()
+//     // db.Close()
 
-    var products []dataTypes.Product
-    db.Find(&products)
-    fmt.Println("{}", products)
+//     var products []dataTypes.Product
+//     db.Find(&products)
+//     fmt.Println("{}", products)
 
-    json.NewEncoder(w).Encode(products)
-}
+//     json.NewEncoder(w).Encode(products)
+// }
 
-func CreateProduct(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("Endpoint Hit: CreateProduct")
+// func CreateProduct(w http.ResponseWriter, r *http.Request) {
+//     fmt.Println("Endpoint Hit: CreateProduct")
 
-    // dsn := "host=localhost user=postgres password=changeme dbname=postgres port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if err != nil {
-        panic("failed to connect database")
-    }
-    // defer db.Close()
+//     // dsn := "host=localhost user=postgres password=changeme dbname=postgres port=5432 sslmode=disable"
+// 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+//     if err != nil {
+//         panic("failed to connect database")
+//     }
+//     // defer db.Close()
 
-    reqBody, _ := ioutil.ReadAll(r.Body)
-    var product dataTypes.Product 
-    json.Unmarshal(reqBody, &product)
+//     reqBody, _ := ioutil.ReadAll(r.Body)
+//     var product dataTypes.Product 
+//     json.Unmarshal(reqBody, &product)
 
-    db.Create(&dataTypes.Product{Code: product.Code, Price: product.Price})
+//     db.Create(&dataTypes.Product{Code: product.Code, Price: product.Price})
 
-    fmt.Fprintf(w, "New Product Successfully Created")
-}
+//     fmt.Fprintf(w, "New Product Successfully Created")
+// }
