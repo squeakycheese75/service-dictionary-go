bee33f67777e061455b841a5bcfd975f18c41ac4
Jamie <james_wooltorton@hotmail.com>
2021-05-20 19:26:32 +0100

    Data following Request/Response pattern with RabbitMQ

--- simple-api/controllers/data.go
+++ simple-api/controllers/data.go
@@ -3,18 +3,87 @@ package controllers
 import (
 	"net/http"
 
-	"github.com/squeakycheese75/service-dictionary-go/simple-api/data"
+	"log"
+
+	"github.com/gorilla/mux"
+	satori_uuid "github.com/satori/go.uuid"
 	"github.com/squeakycheese75/service-dictionary-go/simple-api/env"
 	"github.com/squeakycheese75/service-dictionary-go/simple-api/utils"
+	"github.com/streadway/amqp"
 )
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func requestDataRpc(uuid string) (res string, err error) {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		true,  // exclusive
+		false, // noWait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+    corrId := satori_uuid.NewV4().String()
+
+	err = ch.Publish(
+		"",          // exchange
+		"sd_orchestrator", // routing key
+		false,       // mandatory
+		false,       // immediate
+		amqp.Publishing{
+			ContentType:   "text/plain",
+			CorrelationId: corrId,
+			ReplyTo:       q.Name,
+			Body:          []byte(uuid),
+		})
+	failOnError(err, "Failed to publish a message")
+
+	for d := range msgs {
+		if corrId == d.CorrelationId {
+			res = string(d.Body)
+			failOnError(err, "Failed to convert body to integer")
+			break
+		}
+	}
+	return
+}
 
 func GetData(env *env.Env) func(http.ResponseWriter, *http.Request) {
 	return func(res http.ResponseWriter, req *http.Request) {
-		var entity []data.DataSet
-		if result := env.DB.Find(&entity); result.Error != nil {
-			utils.RespondWithError(res, http.StatusInternalServerError, result.Error.Error())
+		vars := mux.Vars(req)
+		uuid := vars["id"]
+
+		resval, err := requestDataRpc(uuid)
+		if err != nil {
+			utils.RespondWithError(res, http.StatusServiceUnavailable, "Unable to request data from orchestrator")
 			return
 		}
-		utils.RespondWithJSON(res, http.StatusOK, entity)
+		// Then look up dataset with uuid
+		utils.RespondWithJSON(res, http.StatusOK, resval)
 	}
 }
