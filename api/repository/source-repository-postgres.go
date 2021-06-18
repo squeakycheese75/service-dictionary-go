e1d1aaeee133b47329cd9e0b7c2ee85567dd7e12
Jamie Wooltorton <james_wooltorton@hotmail.com>
2021-06-18 15:13:17 +0100

    Adding postgre repository

---
+++ api/repository/source-repository-postgres.go
@@ -0,0 +1,65 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/squeakycheese75/service-dictionary-go/api/data"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type repo_postgres struct{}
+
+var (
+	db_postgres *gorm.DB
+)
+
+// Constructor
+func NewPostgresSourceRepository(dsn string) SourceRepository {
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db_postgres = db
+	return &repo_postgres{}
+}
+
+func (*repo_postgres) Save(source *data.Source) (*data.Source, error) {
+	if result := db_postgres.Create(&source); result.Error != nil {
+		return nil, result.Error
+	}
+	fmt.Println(source.ID)
+	return source, nil
+}
+
+func (*repo_postgres) FindAll() ([]data.Source, error) {
+	var sources []data.Source
+	db_sqlite.Find(&sources)
+	if result := db_postgres.Find(&sources); result.Error != nil {
+		return nil, result.Error
+	}
+	return sources, nil
+}
+
+func (*repo_postgres) Update(source *data.Source) (*data.Source, error) {
+	if result := db_postgres.Save(&source); result.Error != nil {
+		return nil, result.Error
+	}
+	return source, nil
+}
+
+func (*repo_postgres) Find(id string) (*data.Source, error) {
+	var source data.Source
+	if result := db_postgres.First(&source, id); result.Error != nil {
+		return nil, result.Error
+	}
+	return &source, nil
+}
+
+func (*repo_postgres) Delete(id string) (bool, error) {
+	if err := db_postgres.Delete(&data.Source{}, id).Error; err != nil {
+		return false, err
+	}
+	return true, nil
+}
--- api/repository/source_repository_sqlite.go
+++ api/repository/source_repository_sqlite.go
@@ -8,61 +8,58 @@ import (
 	"gorm.io/gorm"
 )
 
-type repo struct{}
+type repo_sqlite struct{}
 
 var (
-	db *gorm.DB
+	db_sqlite *gorm.DB
 )
 
-func connect(name string) *gorm.DB {
+// Constructor
+func NewSqlLiteSourceRepository(name string) SourceRepository {
+
 	db, err := gorm.Open(sqlite.Open(name), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 	fmt.Println("Connected to sqllite db ..")
-	return db
+	db_sqlite = db
+	return &repo_sqlite{}
 }
 
-// Constructor
-func NewSqlLiteSourceRepository(name string) SourceRepository {
-	db = connect(name)
-	return &repo{}
-}
-
-func (*repo) Save(source *data.Source) (*data.Source, error) {
-	if result := db.Create(&source); result.Error != nil {
+func (*repo_sqlite) Save(source *data.Source) (*data.Source, error) {
+	if result := db_sqlite.Create(&source); result.Error != nil {
 		return nil, result.Error
 	}
 	fmt.Println(source.ID)
 	return source, nil
 }
 
-func (*repo) FindAll() ([]data.Source, error) {
+func (*repo_sqlite) FindAll() ([]data.Source, error) {
 	var sources []data.Source
-	db.Find(&sources)
-	if result := db.Find(&sources); result.Error != nil {
+	db_sqlite.Find(&sources)
+	if result := db_sqlite.Find(&sources); result.Error != nil {
 		return nil, result.Error
 	}
 	return sources, nil
 }
 
-func (*repo) Update(source *data.Source) (*data.Source, error) {
-	if result := db.Save(&source); result.Error != nil {
+func (*repo_sqlite) Update(source *data.Source) (*data.Source, error) {
+	if result := db_sqlite.Save(&source); result.Error != nil {
 		return nil, result.Error
 	}
 	return source, nil
 }
 
-func (*repo) Find(id string) (*data.Source, error) {
+func (*repo_sqlite) Find(id string) (*data.Source, error) {
 	var source data.Source
-	if result := db.First(&source, id); result.Error != nil {
+	if result := db_sqlite.First(&source, id); result.Error != nil {
 		return nil, result.Error
 	}
 	return &source, nil
 }
 
-func (*repo) Delete(id string) (bool, error) {
-	if err := db.Delete(&data.Source{}, id).Error; err != nil {
+func (*repo_sqlite) Delete(id string) (bool, error) {
+	if err := db_sqlite.Delete(&data.Source{}, id).Error; err != nil {
 		return false, err
 	}
 	return true, nil
